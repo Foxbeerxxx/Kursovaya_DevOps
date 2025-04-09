c485cf5e96e38aae73a6b89598f3e8e792756195
foxbeerxxx <tatarincev0810@gmail.com>
2025-04-09 22:21:37 +0300

    new file04

---
+++ file04/variables.tf
@@ -0,0 +1,15 @@
+variable "flow" {
+  type    = string
+  default = "24-01"
+}
+
+variable "cloud_id" {
+  type    = string
+  default = "b1gvjpk4qbrvling8qq1"
+}
+variable "folder_id" {
+  type    = string
+  default = "b1gse67sen06i8u6ri78"
+}
+
+
